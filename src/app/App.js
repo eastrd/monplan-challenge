35c3bf1118bf70c5fba5e424569dd47bb857961d
eastrd <yjia285@student.monash.edu>
2017-11-20 00:34:05 +1100

    Included SemanticUI CSS

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,12 @@ import FetchAllComponent from "../components/fetchAll"
 
 class App extends Component {
   dataShareCallBack = (allUnitData) => {
-    
+
   }
   render () {
     return (
       <div className='App'>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Monash Handbook Unit Viewer</h1>
